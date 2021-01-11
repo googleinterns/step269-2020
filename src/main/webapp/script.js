9c31d5824015876a15acb10fa445c209897dc67b
Rachel Schroder <rachel.schroder@bigpond.com>
2021-01-11 06:21:14 +0000

    Add functionality to collapse and expand sidebar

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -82,3 +82,21 @@ function searchLocation() {
 
     changeBackgroundImage(getImagePath());
 }
+
+function closeSidebar() {
+    document.getElementById("close-sidebar").style.display = "none";
+    document.getElementById("open-sidebar").style.display = "inline-block";
+
+    // document.getElementById("sidebar").style.display = "none";
+
+    document.getElementById("sidebar").style.visibility = "hidden";
+}
+
+function openSidebar() {
+    document.getElementById("close-sidebar").style.display = "inline-block";
+    document.getElementById("open-sidebar").style.display = "none";
+
+    // document.getElementById("sidebar").style.display = "inline-block;";
+
+    document.getElementById("sidebar").style.visibility = "visible";
+}
