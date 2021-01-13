496897b3aaad3dc017ad9accda2ac2a03907e936
Rachel Schroder <rachel.schroder@bigpond.com>
2021-01-13 05:49:54 +0000

    Add map widget, page formatting currently broken

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -29,6 +29,15 @@ function getImagePath() {
     return path;
 }
 
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
 function toggleAQLayer() {
     const checkbox = document.getElementById("aq-layer-toggle");
     
