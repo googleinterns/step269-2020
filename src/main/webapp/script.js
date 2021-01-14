aff8a227c1b6cb6a5cfdb0f0dca9b45bdbef5174
Rachel Schroder <rachel.schroder@bigpond.com>
2021-01-14 02:55:32 +0000

    Add toggle-able aq layer layer with fake dataset

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -30,18 +30,47 @@ function getImagePath() {
 }
 
 let map;
+let aqLayer;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 37.775, lng: -122.434 },
+        zoom: 8,
+        // center: { lat: -34.397, lng: 150.644 },
+        //zoom: 8,
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+        position: google.maps.ControlPosition.TOP_RIGHT,
+        },
+        streetViewControl: false,
+        fullscreenControl: false,
+    });
+
+    const heatmapData = [
+        {location: new google.maps.LatLng(37.782, -122.447), weight: 0.5},
+        new google.maps.LatLng(37.782, -122.445),
+        {location: new google.maps.LatLng(37.782, -122.443), weight: 2},
+        {location: new google.maps.LatLng(37.782, -122.441), weight: 3},
+        {location: new google.maps.LatLng(37.782, -122.439), weight: 2},
+        new google.maps.LatLng(37.782, -122.437),
+        {location: new google.maps.LatLng(37.782, -122.435), weight: 0.5},
+
+        {location: new google.maps.LatLng(37.785, -122.447), weight: 3},
+        {location: new google.maps.LatLng(37.785, -122.445), weight: 2},
+        new google.maps.LatLng(37.785, -122.443),
+        {location: new google.maps.LatLng(37.785, -122.441), weight: 0.5},
+        new google.maps.LatLng(37.785, -122.439),
+        {location: new google.maps.LatLng(37.785, -122.437), weight: 2},
+        {location: new google.maps.LatLng(37.785, -122.435), weight: 3}
+    ];
+    aqLayer = new google.maps.visualization.HeatmapLayer({
+        data: heatmapData
+    });
+    aqLayer.setMap(map);
 }
 
 function toggleAQLayer() {
-    const checkbox = document.getElementById("aq-layer-toggle");
-    
-    aqLayerOn = checkbox.checked;
+    aqLayer.setMap(aqLayer.getMap() ? null : map);
 
     changeBackgroundImage(getImagePath());
 }
