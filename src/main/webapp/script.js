d58251c9cd53da5fa31de6553061e2b595fb183a
Rachel Schroder <rachel.schroder@bigpond.com>
2021-01-17 23:23:26 +0000

    Increase zoom level to more clearly show heatmap

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -32,7 +32,7 @@ function aqLayerControl(controlDiv) {
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 37.775, lng: -122.434 },
-        zoom: 8,
+        zoom: 13,
         // zoom for nsw view
         // center: { lat: -34.397, lng: 150.644 },
         //zoom: 8,
