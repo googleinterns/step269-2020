9b06fa817de8730b51bf6c4b7eb6f345c0b9abea
Rachel Schroder <rachel.schroder@bigpond.com>
2021-01-18 01:46:09 +0000

    Move map control css into style.css

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -2,25 +2,13 @@ let map;
 let aqLayer;
 
 function aqLayerControl(controlDiv) {
-    // Set CSS for the control border.
     const controlUI = document.createElement("div");
-    controlUI.style.backgroundColor = "#fff";
-    controlUI.style.border = "2px solid #fff";
-    controlUI.style.borderRadius = "3px";
-    controlUI.style.boxShadow = "0 2px 6px rgba(0,0,0,.3)";
-    controlUI.style.cursor = "pointer";
-    controlUI.style.margin = "10px";
-    controlUI.style.textAlign = "center";
+    controlUI.className = "mapControlUI";
     controlUI.title = "Click to toggle the AQ layer";
     controlDiv.appendChild(controlUI);
-    // Set CSS for the control interior.
+
     const controlText = document.createElement("div");
-    controlText.style.color = "rgb(25,25,25)";
-    controlText.style.fontFamily = "Roboto,Arial,sans-serif";
-    controlText.style.fontSize = "16px";
-    controlText.style.lineHeight = "38px";
-    controlText.style.paddingLeft = "5px";
-    controlText.style.paddingRight = "5px";
+    controlText.className = "mapControlText";
     controlText.innerHTML = "Toggle AQ Layer";
     controlUI.appendChild(controlText);
 
