acac0257ea281720eb045c2a9e409e7e5c3d2fc8
Rachel Schroder <rachel.schroder@bigpond.com>
2021-01-20 05:32:08 +0000

    Add comment

--- src/main/java/com/google/sps/data/AQDataPoint.java
+++ src/main/java/com/google/sps/data/AQDataPoint.java
@@ -1,5 +1,8 @@
 package com.google.sps.data;
 
+/**
+ * This class represents the unified data format for AQ data passed to the client.
+ */
 public class AQDataPoint {
     private int lat;
     private int lng;
