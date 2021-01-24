09e4dd7c29b4d1573e44e44fbbf42b5a03162d58
Rosanna Liu <mushroompotato123@gmail.com>
2021-01-24 15:48:06 +0000

    sucessful run and print of a map of govSiteDetails, and printing the coordiantes from a given siteId

---
+++ src/main/java/com/google/sps/data/Coordinates.java
@@ -0,0 +1,35 @@
+package com.google.sps.data;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.gson.annotations.SerializedName;
+
+/** 
+ * This class represents the coordinates of a site location provided by NSW GOV API for each site avaialble for fitering.
+ */
+public class Coordinates {
+  @SerializedName(value = "Longitude")
+  private final double lng;
+
+  @SerializedName(value = "Latitude")
+  public double lat;
+
+  
+  //combined coordinates
+  public Coordinates(double lng, double lat) {
+    this.lng = lng;
+    this.lat = lat;
+  }  
+
+  //No arg constructor to help differentiate a initialised class with invalid values 
+  public Coordinates() {
+    this.lng = -1;
+    this.lat = -1;
+  }
+
+  @Override
+  public String toString() {
+    return "lng: " + lng + " lat: " + lat;  
+  }  
+}
