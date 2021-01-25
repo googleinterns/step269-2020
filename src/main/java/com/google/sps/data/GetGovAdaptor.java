f51f486a8965e2289af35e0c52a8aa86684729c7
Rosanna Liu <mushroompotato123@gmail.com>
2021-01-25 02:00:19 +0000

    this is a testing branch, code works

--- src/main/java/com/google/sps/data/GetGovAdaptor.java
+++ src/main/java/com/google/sps/data/GetGovAdaptor.java
@@ -78,7 +78,6 @@ public class GetGovAdaptor {
 
       //System.out.println(map);
       //System.out.println(map.get(765).lat);
-
     } else {
       System.out.println("GET request did not work");
     }
