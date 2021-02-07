d429a053cc228e193d43a65d9203c5966b83d8af
Rachel Schroder <rachel.schroder@bigpond.com>
2021-02-07 23:16:57 +0000

    Refactor GriddedData to use map based format and degrees based resolution

--- src/main/java/com/google/sps/data/GriddedData.java
+++ src/main/java/com/google/sps/data/GriddedData.java
@@ -1,42 +1,41 @@
 package com.google.sps.data;
 
+import java.util.HashMap;
+
 /**
  * This class represents the AQI data grid used by the client for visualisation.
- * This grid is a linear approximation of the latitude and longitude, which may be
- * inaccurate due to the curvature of the earth
+ * This grid is a linear approximation of the latitude and longitude, which may
+ * be inaccurate due to the curvature of the earth
  */
 public class GriddedData {
-  public int resolution; // the width & height of each grid cell, stored in metres
-  public double[][] data;
+  public int aqDataPointsPerDegree; // The number of cells it takes to cover a degree of lng/lat
+  
+  public HashMap<Integer,HashMap<Integer,Double>> data;
 
-  // the coordinates of the top left corner of the grid
-  public Coordinates origin;
-
-  public GriddedData(int numColumns, int numRows, int resolution, Coordinates originCoords) {
-    this.resolution = resolution;
-    this.origin = originCoords;
-    this.data = new double[numRows][numColumns];
+  public GriddedData(int aqDataPointsPerDegree) {
+    this.aqDataPointsPerDegree = aqDataPointsPerDegree;
+    this.data = new HashMap<Integer,HashMap<Integer,Double>>();
   }
 
-  public GriddedData(GridCell[][] cells, int resolution, Coordinates originCoords) {
-    int numRows = cells.length;
-    int numColumns = cells[0].length;
-    this.resolution = resolution;
-    this.origin = originCoords;
-    this.data = new double[numRows][numColumns];
+//   public GriddedData(GridCell[][] cells, int aqDataPointsPerDegree, Coordinates originCoords) {
+//     int numRows = cells.length;
+//     int numColumns = cells[0].length;
+//     this.aqDataPointsPerDegree = aqDataPointsPerDegree;
+//     this.origin = originCoords;
+//     this.data = new HashMap<Integer,HashMap<Integer,Double>>();
 
-    for (int rowNum = 0; rowNum < cells.length; rowNum++) {
-      GridCell[] row = cells[rowNum];
-      for (int colNum = 0; colNum < row.length; colNum++) {
-        GridCell cell = row[colNum];
-        if (cell == null) {
-          // There were no data points for this cell
-          // An AQI of 0 will not be displayed by the visualisation layer
-          data[rowNum][colNum] = 0;
-        } else {
-          data[rowNum][colNum] = cell.averageAQI;
-        }
-      }
-    }
-  }
+//     for (int rowNum = 0; rowNum < cells.length; rowNum++) {
+//       GridCell[] row = cells[rowNum];
+//       for (int colNum = 0; colNum < row.length; colNum++) {
+//         GridCell cell = row[colNum];
+//         if (cell == null) {
+//           // There were no data points for this cell
+//           // An AQI of 0 will not be displayed by the visualisation layer
+//           data[rowNum][colNum] = 0;
+//         } else {
+//           data[rowNum][colNum] = cell.averageAQI;
+//         }
+//       }
+//     }
+//   }
 }
