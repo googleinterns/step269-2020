355aaff71a88b525748a7728a3c2be951bf6c035
Rachel Schroder <rachel.schroder@bigpond.com>
2021-02-08 03:27:18 +0000

    Style fix

--- src/main/java/com/google/sps/data/GriddedData.java
+++ src/main/java/com/google/sps/data/GriddedData.java
@@ -9,10 +9,10 @@ import java.util.HashMap;
  */
 public class GriddedData {
   public int aqDataPointsPerDegree; // The number of cells it takes to cover a degree of lng/lat
-  public HashMap<Integer,HashMap<Integer,Double>> data;
+  public HashMap<Integer, HashMap<Integer, Double>> data;
 
   public GriddedData(int aqDataPointsPerDegree) {
     this.aqDataPointsPerDegree = aqDataPointsPerDegree;
-    this.data = new HashMap<Integer,HashMap<Integer,Double>>();
+    this.data = new HashMap<Integer, HashMap<Integer, Double>>();
   }
 }
