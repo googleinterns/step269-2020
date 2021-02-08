9feb42b1c506b25d4f1372f2ff958850398ad0eb
Rachel Schroder <rachel.schroder@bigpond.com>
2021-02-09 10:08:08 +1100

    Small changes to remove warnings

--- src/main/java/com/google/sps/data/HTTPStatusCodeException.java
+++ src/main/java/com/google/sps/data/HTTPStatusCodeException.java
@@ -4,6 +4,8 @@ package com.google.sps.data;
  * A Custom Exception class for a HTTP Status Code that is not =200 OK.
  */
 public class HTTPStatusCodeException extends Exception {
+    private static final long serialVersionUID = -5237033659818337029L;
+
     public HTTPStatusCodeException(String errorMessage) {
         super(errorMessage);
     }
